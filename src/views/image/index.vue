d1d60bd44d98e998447f3bc03ec1178fa0a9d520
zhangsan <zhangsan@sohu.com>
2020-12-04 21:03:59 +0800

    refactor img vue split imagelist component

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -10,192 +10,26 @@
         <!-- /面包屑路径导航 -->
         <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
       </div>
-      <div class="action-head" style="padding-bottom: 20px;">
-        <el-radio-group
-        v-model="collect"
-        size="mini"
-        @change="loadImages(1)"
-        >
-          <el-radio-button :label="false">全部</el-radio-button>
-          <el-radio-button :label="true">收藏</el-radio-button>
-        </el-radio-group>
-        <el-button
-          size="mini"
-          type="success"
-          @click="dialogUploadVisible = true"
-        >上传素材</el-button>
-      </div>
-      <!-- 素材列表 -->
-      <el-row :gutter="10">
-        <el-col
-        :xs="12" :sm="6" :md="6" :lg="4"
-        v-for="(img, index) in images"
-        :key="index"
-        class="image.item"
-        >
-          <el-image
-            style="height: 100px"
-            :src="img.url"
-            fit="cover"
-          ></el-image>
-          <div class="image-action">
-            <!-- <i class="el-icon-star-on"></i> -->
-            <!-- <i
-              :class="{
-                'el-icon-star-on': img.is_collected,
-                'el-icon-star-off': !img.is_collected
-              }"
-              @click="onCollect(img)"
-            ></i> -->
-            <el-button
-              type="warning"
-              :icon="img.is_collected ? 'el-icon-star-on' : 'el-icon-star-off'"
-              circle
-              size="small"
-              @click="onCollect(img)"
-              :loading="img.loading"
-            >
-            </el-button>
-            <!-- <i class="el-icon-delete-solid"></i> -->
-            <el-button
-              type="danger"
-              icon="el-icon-delete-solid"
-              circle
-              size="small"
-              @click="onDelete(img)"
-              :loading="img.loading"
-            >
-            </el-button>
-          </div>
-        </el-col>
-      </el-row>
-      <!-- 数据分页 -->
-      <el-pagination
-        background
-        layout="prev, pager, next"
-        :total="totalCount"
-        :page-size="pageSize"
-        :current-page.sync="page"
-        @current-change="onPageChange"
-      >
-      </el-pagination>
-      <!-- /素材列表 -->
-      <el-dialog
-        title="上传素材"
-        :visible.sync="dialogUploadVisible"
-        :append-to-body="true"
-      >
-        <el-upload
-          class="upload-demo"
-          drag
-          action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
-          :headers="uploadHeaders"
-          name="image"
-          multiple
-          :show-file-list="false"
-          :on-success="onUploadSuccess"
-        >
-          <i class="el-icon-upload"></i>
-          <div class="el-upload_text">将文件拖到此处，或<em>点击上传</em></div>
-          <div class="el-upload_tip" slot="tip">智能上传jpg/png文件，且不超过500kb</div>
-        </el-upload>
-      </el-dialog>
+      <image-list />
     </el-card>
   </div>
 </template>
 
 <script>
-import { getImages, collectImage, deleteImage } from '@/api/image'
+import ImageList from './components/image-list'
 export default {
   name: 'ImageIndex',
-  components: {},
+  components: { ImageList },
   props: {},
   data () {
-    const user = JSON.parse(window.localStorage.getItem('user'))
-    return {
-      collect: false,
-      images: [],
-      dialogUploadVisible: false,
-      uploadHeaders: {
-        Authorization: `Bearer ${user.token}`
-      },
-      totalCount: 0,
-      pageSize: 7,
-      page: 1
-    }
+    return {}
   },
   computed: {},
   watch: {},
-  created () {
-    this.loadImages(1)
-  },
+  created () {},
   mounted () {},
-  methods: {
-    loadImages (page = 1) {
-      getImages({
-        collect: this.collect,
-        page,
-        per_page: this.pageSize
-      }).then(res => {
-        const results = res.data.data.results
-        results.forEach(img => {
-          img.loading = false
-        })
-        this.images = results
-        this.totalCount = res.data.data.total_count
-      })
-    },
-    onUploadSuccess () {
-      this.dialogUploadVisible = false
-      this.loadImages(this.page)
-      this.$message({
-        type: 'success',
-        message: '上传成功'
-      })
-    },
-    onPageChange (page) {
-      this.loadImages(page)
-    },
-    onCollect (img) {
-      img.loading = true
-      collectImage(img.id, !img.is_collected).then(res => {
-        img.is_collected = !img.is_collected
-        img.loading = false
-      })
-    },
-    onDelete (img) {
-      img.loading = true
-      deleteImage(img.id).then(res => {
-        this.loadImages(this.page)
-        img.loading = false
-      })
-    }
-  }
+  methods: {}
 }
 </script>
-
 <style scoped lang="less">
-.action-head {
-  padding-bottom: 20px;
-  display: flex;
-  justify-content: space-between;
-}
-
-.image-item {
-  position: relative;
-}
-
-.image-action {
-  font-size: 25px;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  color:#fff;
-  height: 40px;
-  background-color: rgba(204,204,204, .5);
-  // position: absolute;
-  bottom: 4px;
-  left: 5px;
-  right: 5px;
-}
 </style>
